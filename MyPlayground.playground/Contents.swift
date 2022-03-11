c7973fc0b7ef7001dcec3495b04e40ed14d85069
graphtobinary <prashant.krishna52@gmail.com>
2022-03-11 09:11:47 +0530

    added first 3 apps

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+   let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+   
+   //The number of letters in alphabet equals 26
+    var h = alphabet.randomElement()
+    var i = alphabet.randomElement()
+    let j = alphabet.randomElement()
+    let k = alphabet.randomElement()
+    let l = alphabet.randomElement()
+    let m = alphabet.randomElement()
+   
+let password = String(h + i)
+   
+   print(password)
