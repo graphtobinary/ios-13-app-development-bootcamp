bc4946499a40c808b0b00c99972ee6735769f9ee
graphtobinary <prashant.krishna52@gmail.com>
2022-03-11 09:26:58 +0530

    dicee & Magin 8

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        let images = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        
+        diceImageView1.image = images.randomElement()
+        diceImageView2.image = images.randomElement()
+
+    }
+    
+}
+
